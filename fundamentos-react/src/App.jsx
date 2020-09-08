d1c4ba4261d2b57e661c0b8ae385a87051d98ae1
Vinicius Schulte <schultev2@gmail.com>
2020-09-08 15:15:40 -0300

    Adicionado componente com filho, passando map para apresentar a lista de filhos

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,18 +1,27 @@
 import './App.css'
 import React from 'react'
 
+import FamiliaMembro from './components/basicos/FamiliaMembro'
+import Familia from './components/basicos/Familia'
 import Card from './components/layout/Card'
 import Aleatorio from './components/basicos/Aleatorio'
 import Fragmento from './components/basicos/Fragmento'
 import ComParametro from './components/basicos/ComParametro'
 import Primeiro from './components/basicos/Primeiro'
 
-export default props => {
+export default () => {
     return (
         <div className="App">
             <h1>Fundamentos React</h1>
             <hr/>
             <div className="Cards">
+                <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                    <Familia sobrenome="Ferreira">
+                        <FamiliaMembro nome="Pedro" />
+                        <FamiliaMembro nome="Ana" />
+                        <FamiliaMembro nome="Gustavo" />
+                    </Familia>
+                </Card>
                 <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                     <Aleatorio min={1} max={60}/>
                 </Card>
