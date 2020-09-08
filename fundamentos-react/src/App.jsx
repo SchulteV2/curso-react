8714cd6a2447bc00c37ca4caf171517ef0aaa1d8
Vinicius Schulte <schultev2@gmail.com>
2020-09-08 14:51:30 -0300

    Ajustado o css do componente Card, passando as cores dos Cards pelo App.jsx

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react'
 
 import Card from './components/layout/Card'
@@ -8,21 +9,23 @@ import Primeiro from './components/basicos/Primeiro'
 
 export default props => {
     return (
-        <div id="app">
+        <div className="App">
             <h1>Fundamentos React</h1>
             <hr/>
-            <Card titulo="#04 - Desafio Aleatório">
-                <Aleatorio min={1} max={60}/>
-            </Card>
-            <Card titulo="#03 - Fragmento">
-                <Fragmento />
-            </Card>
-            <Card titulo="#02 - Com Parâmetro">
-                <ComParametro titulo="Situação do Aluno" aluno="Pedro" nota="9.3" />
-            </Card>
-            <Card titulo="#01 - Primeiro Componente">
-                <Primeiro />
-            </Card>
+            <div className="Cards">
+                <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+                    <Aleatorio min={1} max={60}/>
+                </Card>
+                <Card titulo="#03 - Fragmento" color="#E94C6F">
+                    <Fragmento />
+                </Card>
+                <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+                    <ComParametro titulo="Situação do Aluno" aluno="Pedro" nota="9.3" />
+                </Card>
+                <Card titulo="#01 - Primeiro Componente" color="#588C73">
+                    <Primeiro />
+                </Card>
+            </div>
         </div>
     );
 }
