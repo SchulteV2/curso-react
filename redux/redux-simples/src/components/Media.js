0236fde8cc9bdd7c4cf860fe4f092d0d3a2a0ca8
Vinicius Schulte <schultev2@gmail.com>
2020-10-14 16:13:20 -0300

    Funcionalidade SEM REDUX para fazer média, soma e sorteio pegando os números passados pelo intervalo

--- redux/redux-simples/src/components/Media.js
+++ redux/redux-simples/src/components/Media.js
@@ -3,12 +3,16 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+    // const min = props.min
+    // const max = props.max
+    const { min, max } = props
+    
     return (
         <Card title="Média dos Números" green>
             <div>
                 <span>
                     <span>Resultado:</span>
-                    <strong>5</strong>
+                    <strong>{(max + min) / 2}</strong>
                 </span>
             </div>
         </Card>
