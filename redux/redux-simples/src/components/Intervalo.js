149ff64803b7c0e6c72f306e8b19a57357b486de
Vinicius Schulte <schultev2@gmail.com>
2020-10-15 16:16:11 -0300

    Importada as funções das actions, para alterar os numeros que antes eram fixos

--- redux/redux-simples/src/components/Intervalo.js
+++ redux/redux-simples/src/components/Intervalo.js
@@ -3,19 +3,21 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import Card from './Card'
+import { alterarNumeroMinimo, alterarNumeroMaximo } from '../store/actions/numeros'
 
 const Intervalo = props => {
     const { min, max } = props
+
     return (
         <Card title="Intervalo de Números" red>
             <div className="Intervalo">
                 <span>
                     <strong>Mínimo:</strong>
-                    <input type="number" value={min} readOnly/>
+                    <input type="number" value={min} onChange={e => props.alterarMinimo(+e.target.value)}/>
                 </span>
                 <span>
                     <strong>Máximo:</strong>
-                    <input type="number" value={max} readOnly/>
+                    <input type="number" value={max} onChange={e => props.alterarMaximo(+e.target.value)} />
                 </span>
             </div>
         </Card>
@@ -29,4 +31,19 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps)(Intervalo)
+const mapDispatchToProps = dispatch => {
+    return {
+        alterarMinimo(novoNumero) {
+            // Action Creator -> action
+            const action = alterarNumeroMinimo(novoNumero)
+            dispatch(action)
+        },
+        alterarMaximo(novoNumero) {
+            // Action Creator -> action
+            const action = alterarNumeroMaximo(novoNumero)
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Intervalo)
